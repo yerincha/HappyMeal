9d00f1c353ccfffd0f6b44503d5aec6ad3eefc84
Yerin Cha <lenacha32@gmail.com>
2023-02-19 17:08:07 -0600

    Add model for Recipe

---
+++ src/model/Recipe.ts
@@ -0,0 +1,28 @@
+class Recipe {
+    id: string;
+    title: string;
+    image: string | undefined;
+    isFavorite: boolean = false;
+
+    constructor(id: string, title: string, image: string, isFavorite: boolean) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.isFavorite = isFavorite;
+    }
+
+    public static fromObject(recipeObject: {id: string; title: string; image: string; isFavorite: boolean | null}) {
+        return new Recipe(recipeObject.id, recipeObject.title, recipeObject.image, recipeObject.isFavorite ?? false);
+    }
+
+    public toObject(): object {
+        return {
+            id: this.id,
+            title: this.title,
+            image: this.image,
+            isFavorite: this.isFavorite
+        };
+    }
+}
+
+export default Recipe
