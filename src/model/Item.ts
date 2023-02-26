a66c623729ca9352ed0a20d6c0528f13abb2ddd9
Yerin Cha <lenacha32@gmail.com>
2023-02-26 11:51:24 -0600

    Update Item model

--- src/model/Item.ts
+++ src/model/Item.ts
@@ -1,27 +1,45 @@
-import { Timestamp } from "firebase/firestore";
+import { Timestamp } from 'firebase/firestore';
 
 class Item {
-    name: string | undefined;
-    quantity: number = 0;
+  id: number;
+  name: string | undefined;
+  quantity: number = 0;
+  expiredAt: Timestamp | null;
+
+  constructor(
+    id: number,
+    name: string,
+    quantity: number = 0,
+    expiredAt: Timestamp | null
+  ) {
+    this.id = id;
+    this.name = name;
+    this.quantity = quantity;
+    this.expiredAt = expiredAt;
+  }
+
+  public static fromObject(itemObj: {
+    id: number;
+    name: string;
+    quantity: number | 0;
     expiredAt: Timestamp | null;
+  }) {
+    return new Item(
+      itemObj.id,
+      itemObj.name,
+      itemObj.quantity ?? 0,
+      itemObj.expiredAt
+    );
+  }
 
-    constructor(name: string, quantity: number = 0, expiredAt: Timestamp | null) {
-        this.name = name;
-        this.quantity = quantity;
-        this.expiredAt = expiredAt;
-    }
-
-    public static fromObject(itemObj: {name: string; quantity: number | 0; expiredAt: Timestamp | null}) {
-        return new Item(itemObj.name, itemObj.quantity ?? 0, itemObj.expiredAt);
-    }
-
-    public toObject(): object {
-        return {
-            name: this.name,
-            quantity: this.quantity,
-            expiredAt: this.expiredAt ?? null,
-        };
-    }
+  public toObject(): object {
+    return {
+      id: this.id,
+      name: this.name,
+      quantity: this.quantity,
+      expiredAt: this.expiredAt ?? null,
+    };
+  }
 }
 
-export default Item
+export default Item;
