b0c5190a6325761c77e12ae99350ba506710a3f3
Yerin Cha <lenacha32@gmail.com>
2023-03-05 19:10:46 -0600

    Edit cypress config for coverage analysis

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,13 +11,11 @@ export default defineConfig({
     video: false,
     screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config);
-      // include any other plugin code...
-
-      // It's IMPORTANT to return the config object
-      // with any changed environment variables
-      return config;
+      require('@bahmutov/cypress-code-coverage/plugin')(on, config)
+      return config
     },
+    supportFile: "cypress/support/index.js",
+    supportFolder: "cypress/support",
   },
 
   component: {
