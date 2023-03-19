50403c360a3c7a9f0e5be3ef92002dff6419552e
Yerin Cha <lenacha32@gmail.com>
2023-03-19 16:17:57 -0500

    Edit CardGrid with updated loadedRecipes

--- src/modules/components/CardGrid.tsx
+++ src/modules/components/CardGrid.tsx
@@ -3,13 +3,17 @@ import GridList from '@mui/material/Grid';
 import CardItem from './CardItem';
 import Recipe from '../../model/Recipe';
 
-export default function CardGrid(props: { data: Array<Recipe>, loadedRecipes: Array<Recipe> }) {
+export default function CardGrid(props: {
+  data: Array<Recipe>;
+  loadedRecipes: Map<number, Recipe>;
+}) {
   return (
     <div className='CardGrid'>
       <GridList container item xs={12} spacing={0}>
-        {props.data.map((tile, i) => (
-          <CardItem tile={tile} key={i} loadedRecipes={props.loadedRecipes}/>
-        ))}
+        {props.loadedRecipes.size > 0 &&
+          Array.from(props.loadedRecipes.values()).map((tile: Recipe, i) => (
+            <CardItem tile={tile} key={i} loadedRecipes={props.loadedRecipes} />
+          ))}
       </GridList>
     </div>
   );
