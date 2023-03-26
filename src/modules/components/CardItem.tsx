ef339a23adb8e5035f97b4f91ad9ab052e381c62
Yerin Cha <lenacha32@gmail.com>
2023-03-25 19:15:54 -0500

    Update CardItem with antd

--- src/modules/components/CardItem.tsx
+++ src/modules/components/CardItem.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
-import Typography from './Typography';
 import axios from 'axios';
 import { API_KEY } from '../../credentials';
 import APIService from '../../api/APIService';
 import { useAuth } from '../../context/AuthContext';
 import Recipe from '../../model/Recipe';
+import { Button,Card } from 'antd';
+import { Stack } from '@mui/material';
+
+const { Meta } = Card;
 
 export default function CardGrid(props: {
   tile: Recipe;
   loadedRecipes: Map<number, Recipe>;
 }) {
   const { user } = useAuth();
+
+  const [isSaving, setIsSaving] = React.useState<boolean>(false);
 
   const handleDetailClick = () => {
     const options = {
@@ -34,57 +38,44 @@ export default function CardGrid(props: {
       });
   };
 
-  const handleAddClick = (event: Event) => {
-    event.stopPropagation();
+  const handleAddToMyListClick = () => {
+    setIsSaving(true);
     const { loadedRecipes, tile } = props;
     if (loadedRecipes.has(tile.id)) {
       return;
     }
     loadedRecipes.set(tile.id, tile);
     APIService.getInstance().setMyRecipes(
-      user.uid,
-      Array.from(loadedRecipes.values())
-    );
-    alert('Saved!');
-  };
-
-  const handleDelete = () => {
-    const { loadedRecipes, tile } = props;
-    loadedRecipes.delete(tile.id);
-    APIService.getInstance().setMyRecipes(
-      user.uid,
-      Array.from(loadedRecipes.values())
-    );
+        user.uid,
+        Array.from(loadedRecipes.values())
+      ).then((res) => {
+        setIsSaving(false);
+      });
   };
 
   return (
-    <Card sx={{ width: 500 }} data-testid='recipeItemCard'>
-      <CardActionArea>
-        <CardMedia
-          component='img'
-          alt='Contemplative Reptile'
-          sx={{ height: 200 }}
-          image={props.tile.image}
-          title='Contemplative Reptile'
-          onClick={handleDetailClick}
+    <Card
+      cover={
+        <img
+          alt="example"
+          src={props.tile.image}
         />
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant='body2'
-            component='h2'
-            noWrap
-            data-testid='recipeTitle'
-            onClick={handleDetailClick}
-          >
-            {props.tile.title}
-          </Typography>
-          <Typography onClick={handleAddClick} data-testid='addRecipeButton'>
-            {' '}
-            Add to My List
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      }
+    >
+      <Meta
+        title={props.tile.title}
+      />
+      <Stack marginTop={3} spacing={2}>
+        <Button type="dashed" onClick={handleDetailClick}>See Detail</Button>
+        <Button
+          type="dashed" 
+          loading={isSaving} 
+          onClick={handleAddToMyListClick}>
+          {
+            props.loadedRecipes.has(props.tile.id) ? "✅ Already In My Recipe" : "Add to My Recipe"
+          }
+        </Button>
+      </Stack>
     </Card>
   );
 }
