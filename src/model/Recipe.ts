5fb8b2d09fd8e71616c7153906c2aa77d8c27530
Yerin Cha <lenacha32@gmail.com>
2023-03-25 19:14:22 -0500

    Update Recipe model

--- src/model/Recipe.ts
+++ src/model/Recipe.ts
@@ -3,16 +3,20 @@ class Recipe {
     title: string;
     image: string | undefined;
     isFavorite: boolean = false;
+    isTried: boolean = false;
+    rating: number | undefined;
 
-    constructor(id: number, title: string, image: string, isFavorite: boolean) {
+    constructor(id: number, title: string, image: string, isFavorite: boolean, isTried: boolean, rating: number) {
         this.id = id;
         this.title = title;
         this.image = image;
         this.isFavorite = isFavorite;
+        this.isTried = isTried;
+        this.rating = rating;
     }
 
-    public static fromObject(recipeObject: {id: number; title: string; image: string; isFavorite: boolean | null}) {
-        return new Recipe(recipeObject.id, recipeObject.title, recipeObject.image, recipeObject.isFavorite ?? false);
+    public static fromObject(recipeObject: {id: number; title: string; image: string; isFavorite: boolean | null; isTried: boolean | null; rating: number | null}) {
+        return new Recipe(recipeObject.id, recipeObject.title, recipeObject.image, recipeObject.isFavorite ?? false, recipeObject.isTried ?? false, recipeObject.rating ?? -1);
     }
 
     public toObject(): object {
@@ -20,7 +24,9 @@ class Recipe {
             id: this.id,
             title: this.title,
             image: this.image,
-            isFavorite: this.isFavorite
+            isFavorite: this.isFavorite,
+            isTried: this.isTried ?? false,
+            rating: this.rating ?? null,
         };
     }
 }
