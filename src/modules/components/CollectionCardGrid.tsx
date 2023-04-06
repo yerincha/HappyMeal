73294b625a30a3dbd8f7c515933a6cc45d13fb9b
Yerin Cha <lenacha32@gmail.com>
2023-04-06 17:38:08 -0500

    Update CollectionCardGrid component for adding filter and sort

--- src/modules/components/CollectionCardGrid.tsx
+++ src/modules/components/CollectionCardGrid.tsx
@@ -6,12 +6,29 @@ import Recipe from '../../model/Recipe';
 export default function CollectionCardGrid(props: {
   loadItems: () => void;
   loadedRecipes: Map<number, Recipe>;
+  sortOption: string;
 }) {
+
   return (
     <List
       width={'70%'}
       grid={{ gutter: 10, column: 3 }}
-      dataSource={props.loadedRecipes.values()}
+      dataSource={Array.from(props.loadedRecipes.values()).sort((i1, i2) => {
+        switch (props.sortOption) {
+          case 'no':
+          case '':
+            return -1;
+          case 'name_asc':
+            return i1.title.localeCompare(i2.title);
+          case 'name_dsc':
+            return -i1.title.localeCompare(i2.title);
+          case 'added_datetime_asc':
+            return (i1.addedDateTime?.seconds ?? 0) - (i2.addedDateTime?.seconds ?? 0);
+          case 'added_datetime_dsc':
+            return (i2.addedDateTime?.seconds ?? 0) - (i1.addedDateTime?.seconds ?? 0);
+        }
+        return -1;
+      })}
       renderItem={(item) => (
         <List.Item>
           <CollectionCardItem
