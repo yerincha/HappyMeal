49bcd7181aca159cfb0be177d889028e2674e278
Yerin Cha <lenacha32@gmail.com>
2023-04-06 17:37:42 -0500

    Update Recipe model

--- src/model/Recipe.ts
+++ src/model/Recipe.ts
@@ -1,3 +1,5 @@
+import { Timestamp } from "firebase/firestore";
+
 class Recipe {
     id: number;
     title: string;
@@ -5,18 +7,20 @@ class Recipe {
     isFavorite: boolean = false;
     isTried: boolean = false;
     rating: number | undefined;
+    addedDateTime: Timestamp | undefined;
 
-    constructor(id: number, title: string, image: string, isFavorite: boolean, isTried: boolean, rating: number) {
+    constructor(id: number, title: string, image: string, isFavorite: boolean, isTried: boolean, rating: number, addedDateTime: Timestamp) {
         this.id = id;
         this.title = title;
         this.image = image;
         this.isFavorite = isFavorite;
         this.isTried = isTried;
         this.rating = rating;
+        this.addedDateTime = addedDateTime;
     }
 
-    public static fromObject(recipeObject: {id: number; title: string; image: string; isFavorite: boolean | null; isTried: boolean | null; rating: number | null}) {
-        return new Recipe(recipeObject.id, recipeObject.title, recipeObject.image, recipeObject.isFavorite ?? false, recipeObject.isTried ?? false, recipeObject.rating ?? -1);
+    public static fromObject(recipeObject: {id: number; title: string; image: string; isFavorite: boolean | null; isTried: boolean | null; rating: number | null; addedDateTime: Timestamp | null;}) {
+        return new Recipe(recipeObject.id, recipeObject.title, recipeObject.image, recipeObject.isFavorite ?? false, recipeObject.isTried ?? false, recipeObject.rating ?? -1, recipeObject.addedDateTime ?? Timestamp.now());
     }
 
     public toObject(): object {
@@ -27,6 +31,7 @@ class Recipe {
             isFavorite: this.isFavorite,
             isTried: this.isTried ?? false,
             rating: this.rating ?? null,
+            addedDateTime: this.addedDateTime ?? Timestamp.now(),
         };
     }
 }
