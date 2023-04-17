b75aca0af44b08f359edb93caadb5aec0e04e56e
Yerin Cha <lenacha32@gmail.com>
2023-04-16 19:24:31 -0500

    Update CardGrid component with pagination

--- src/modules/components/CardGrid.tsx
+++ src/modules/components/CardGrid.tsx
@@ -8,14 +8,15 @@ export default function CardGrid(props: {
   loadedRecipes: Map<number, Recipe>;
 }) {
   return (
-      <List
-        grid={{ gutter: 10, column: 3 }}
-        dataSource={props.data ?? []}
-        renderItem={(item) => (
-          <List.Item>
-            <CardItem tile={item} loadedRecipes={props.loadedRecipes} />
-          </List.Item>
-        )}
-      />
+    <List
+      grid={{ gutter: 10, column: 3 }}
+      dataSource={props.data ?? []}
+      pagination={{ position: 'bottom', align: 'center', defaultPageSize: 6 }}
+      renderItem={(item) => (
+        <List.Item>
+          <CardItem tile={item} loadedRecipes={props.loadedRecipes} />
+        </List.Item>
+      )}
+    />
   );
 }
